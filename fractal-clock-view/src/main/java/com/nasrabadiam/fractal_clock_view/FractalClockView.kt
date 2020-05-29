5b7d9afb8cbdf51d50ca19d57b19f7181faab4ff
Ali Nasrabadi <nasrabadiam@cafebazaar.ir>
2020-05-29 15:37:32 +0430

    Add a View Helper to do calculations stuff

--- fractal-clock-view/src/main/java/com/nasrabadiam/fractal_clock_view/FractalClockView.kt
+++ fractal-clock-view/src/main/java/com/nasrabadiam/fractal_clock_view/FractalClockView.kt
@@ -4,13 +4,9 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.Point
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
-import kotlin.math.PI
-import kotlin.math.cos
-import kotlin.math.sin
 
 class FractalClockView @JvmOverloads constructor(
     context: Context,
@@ -21,6 +17,7 @@ class FractalClockView @JvmOverloads constructor(
     private var minute: Int = 0
     private var second: Int = 0
     private var milliSeconds: Int = 0
+    private val clockViewHelper = ClockViewHelper()
 
     private val paintOfHour = Paint().apply {
         color = Color.RED
@@ -63,70 +60,30 @@ class FractalClockView @JvmOverloads constructor(
     }
 
     private fun getCenterOfViewPoint(): Point {
-        return Point((width / 2f).toInt(), (height / 2f).toInt())
+        return Point(width / 2f, height / 2f)
     }
 
     override fun onDraw(canvas: Canvas?) {
         val centerOfView = getCenterOfViewPoint()
-        val hourEndPoint = getHourEndPoint(hour, minute, second)
-        val minuteEndPoint = getMinuteEndPoint(minute, second)
-        val secondEndPoint = getSecondEndPoint(second, milliSeconds)
-
-        canvas?.apply {
-            drawLine(
-                centerOfView.x.toFloat(),
-                centerOfView.y.toFloat(),
-                hourEndPoint.x.toFloat(),
-                hourEndPoint.y.toFloat(),
-                paintOfHour
-            )
-            drawLine(
-                centerOfView.x.toFloat(),
-                centerOfView.y.toFloat(),
-                minuteEndPoint.x.toFloat(),
-                minuteEndPoint.y.toFloat(),
-                paintOfMinute
-            )
-            drawLine(
-                centerOfView.x.toFloat(),
-                centerOfView.y.toFloat(),
-                secondEndPoint.x.toFloat(),
-                secondEndPoint.y.toFloat(),
-                paintOfSecond
+        val lines = clockViewHelper.getLegLinesToDraw(hour, minute, second, milliSeconds, centerOfView)
+        lines.forEach {
+            val paint = getPaint(it)
+            canvas?.drawLine(
+                it.startPoint.x,
+                it.startPoint.y,
+                it.endPoint.x,
+                it.endPoint.y,
+                paint
             )
         }
         super.onDraw(canvas)
     }
 
-    private fun getHourEndPoint(hour: Int, minute: Int, second: Int): Point {
-        val halfOfView = minOf(height, width) / 2
-        val hourLegLength = halfOfView * .4
-        val hourDeg = hour * ONE_HOUR_DEGREE_IN_CIRCLE + minute * ONE_MIN_DEGREE_OF_HOUR_IN_CIRCLE + second * ONE_SEC_DEGREE_OF_HOUR_IN_CIRCLE
-        return getEndPoint(hourDeg, hourLegLength)
+    private fun getPaint(it: Line) = when (it.legType) {
+        LegType.HOUR -> paintOfHour
+        LegType.MINUTE -> paintOfMinute
+        LegType.SECOND -> paintOfSecond
     }
-
-    private fun getMinuteEndPoint(minute: Int, second: Int): Point {
-        val halfOfView = minOf(height, width) / 2
-        val minuteLegLength = halfOfView * .5
-        val minuteDeg = minute * ONE_MIN_DEGREE_IN_CIRCLE + second * ONE_SEC_DEGREE_OF_MINUTE_IN_CIRCLE
-        return getEndPoint(minuteDeg, minuteLegLength)
-    }
-
-    private fun getSecondEndPoint(second: Int, millieSeconds: Int): Point {
-        val halfOfView = minOf(height, width) / 2
-        val secondLegLength = halfOfView * .6
-        val secondDeg = second * ONE_SEC_DEGREE_IN_CIRCLE + millieSeconds * (360.0 / 60 / 1000)
-        return getEndPoint(secondDeg, secondLegLength)
-    }
-
-    private fun getEndPoint(degree: Double, legLength: Double): Point {
-        val secondRadian = getInRadian(degree)
-        val endPointX = (sin(secondRadian) * legLength) + (width / 2)
-        val endPointY = -(cos(secondRadian) * legLength) + (height / 2)
-        return Point(endPointX.toInt(), endPointY.toInt())
-    }
-
-    private fun getInRadian(degree: Double) = degree * PI / 180.0
 
     private fun measureDimension(desiredSize: Int, measureSpec: Int): Int {
         var result: Int
@@ -148,11 +105,5 @@ class FractalClockView @JvmOverloads constructor(
 
     companion object {
         const val TAG = "FractalClockView"
-        const val ONE_HOUR_DEGREE_IN_CIRCLE = 360.0 / 12.0
-        const val ONE_MIN_DEGREE_OF_HOUR_IN_CIRCLE = 360.0 / (12.0 * 60.0)
-        const val ONE_MIN_DEGREE_IN_CIRCLE = 360.0 / 60.0
-        const val ONE_SEC_DEGREE_IN_CIRCLE = 360.0 / 60.0
-        const val ONE_SEC_DEGREE_OF_HOUR_IN_CIRCLE = 360.0 / (12.0 * 60.0 * 60.0)
-        const val ONE_SEC_DEGREE_OF_MINUTE_IN_CIRCLE = 360.0 / (60.0 * 60.0)
     }
 }
